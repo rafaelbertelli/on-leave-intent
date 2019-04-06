46e9464f481d50d5dcd9e0b3481e425e7bab0dd0
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-06 12:51:59 -0300

    Component refactoring using test benefits

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ class OnLeaveIntent {
   constructor(callback) {
     this.callback = callback
 
-    this.callback()
+    this.init()
+  }
+
+  init = () => {
+    this.handleMouseOut()
+  }
+
+  checkOutOfBounds = event => event.relatedTarget === null && this.callback()
+
+  handleMouseOut() {
+    document.addEventListener('mouseout', this.checkOutOfBounds)
   }
 }
 
