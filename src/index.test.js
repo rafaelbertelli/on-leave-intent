9d9b06ebb1250a590ce9d1b9e22baedaab63599f
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-06 12:55:14 -0300

    Tests refactoring using test benefits

--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,25 @@
 import OnLeaveIntent from './index'
 
 describe('OnLeaveIntent', () => {
-  it('should return 5', () => {
-    expect(5).toBe(5)
+  let callback
+  let onLeaveIntent
+
+  beforeEach(() => {
+    callback = jest.fn()
+    onLeaveIntent = new OnLeaveIntent(callback)
   })
 
   it('should call a callback function if user go out of the screen', () => {
-    const callback = jest.fn()
-    const onLeaveIntent = new OnLeaveIntent(callback)
-
     // simulate the user leaving the page
     document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
-
     expect(callback).toHaveBeenCalled()
   })
 
   it('should not call a callback function if user stills on the screen', () => {
-    const callback = jest.fn()
-    const onLeaveIntent = new OnLeaveIntent(callback)
-
     // simulate the user on the page
     document.dispatchEvent(
       new MouseEvent('mouseout', { relatedTarget: new EventTarget() })
     )
-
     expect(callback).not.toHaveBeenCalled()
   })
 })
