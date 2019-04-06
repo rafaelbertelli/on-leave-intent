69e11f8df98ffa2bf0b7ac72b743b9397a7ad239
Rafael Borges <rafaelbertelli@gmail.com>
2019-04-06 13:18:47 -0300

    Increase test

--- src/index.test.js
+++ src/index.test.js
@@ -3,23 +3,34 @@ import OnLeaveIntent from './index'
 describe('OnLeaveIntent', () => {
   let callback
   let onLeaveIntent
+  const delay = 1000 // 1000ms === 1s
+  jest.useFakeTimers()
 
   beforeEach(() => {
     callback = jest.fn()
-    onLeaveIntent = new OnLeaveIntent(callback)
+    onLeaveIntent = new OnLeaveIntent(callback, delay)
   })
 
-  it('should call a callback function if user go out of the screen', () => {
+  it('should call the callback function if user go out of the screen', () => {
+    // advance 1s
+    jest.advanceTimersByTime(delay)
     // simulate the user leaving the page
     document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
     expect(callback).toHaveBeenCalled()
   })
 
-  it('should not call a callback function if user stills on the screen', () => {
-    // simulate the user on the page
+  it('should not call the callback function if user stills on the screen', () => {
+    jest.advanceTimersByTime(delay)
     document.dispatchEvent(
       new MouseEvent('mouseout', { relatedTarget: new EventTarget() })
     )
+    expect(callback).not.toHaveBeenCalled()
+  })
+
+  it('should not call the callback function before the delay', () => {
+    // advance half delay
+    jest.advanceTimersByTime(delay / 2)
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
     expect(callback).not.toHaveBeenCalled()
   })
 })
